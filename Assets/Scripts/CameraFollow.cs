248a4c524481c7d678f4a4c55292fbfa641e89ca
HotFury <HotFury@yandex.ru>
2015-02-20 11:35:33 +0200

    cameraFollow vertical and horizontal (with comments)

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,32 +3,32 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
-    public GameObject player;
+    public GameObject player;     // reference to player
     public float cameraOffsetX;
     public float cameraOffsetY;
     public float cameraStepX;
     public float cameraStepY;
-    public float playerSpaceAbove;
-    public float playerSpaceUnder;
-    public float cameraEdgeHor;
+    public float playerSpaceAbove;  // space for player above center of camera (% from height)
+    public float playerSpaceUnder;  // space for player under center of camera (% from height)
+    public float cameraEdgeHor;     // horizontal edge
     public float playerHeight;
 
     private bool cameraCorrectAllowHor = false; // flag allow to chage side
     private bool allowMoveCameraHor = true;
     private bool cameraCorrectAllowVer = false;
-    private bool allowMoveCameraVerUp = false;
-    private bool allowMoveCameraVerDown = false;
+    private bool allowMoveCameraUp = false;
+    private bool allowMoveCameraDown = false;
 
     private Animator anim;
-    private float cameraEdgeUp;
-    private float cameraEdgeDown;
+    private float cameraEdgeUp;    // vertical upo edge
+    private float cameraEdgeDown;  // vertical down edge
 
 
     // Use this for initialization
     void Start()
     {
-        cameraEdgeUp = transform.position.y + (2 * camera.orthographicSize) * playerSpaceAbove / 100 - (playerHeight / 2f); // calculate vertical edge of camera (% of height)
-        cameraEdgeDown = transform.position.y - (2 * camera.orthographicSize) * playerSpaceUnder / 100 + (playerHeight / 2f);
+        cameraEdgeUp = transform.position.y + (2 * camera.orthographicSize) * playerSpaceAbove / 100 - (playerHeight / 2f);    // calculate vertical up edge of camera (% to units)
+        cameraEdgeDown = transform.position.y - (2 * camera.orthographicSize) * playerSpaceUnder / 100 - (playerHeight / 2f);  // calculate vertical down edge of camera (% to units)
         anim = player.GetComponent<Animator>();
     }
 
@@ -36,22 +36,23 @@ public class CameraFollow : MonoBehaviour {
     /* forbid change side and correction position of camera */
     void SetCameraCorrectAllowHorFalse()
     {
-        transform.position = new Vector3(player.transform.position.x + cameraOffsetX, transform.position.y, transform.position.z);
-        cameraCorrectAllowHor = false;
+        transform.position = new Vector3(player.transform.position.x + cameraOffsetX, transform.position.y, transform.position.z);  // correction position of camera
+        cameraCorrectAllowHor = false;  // forbid correct camera horizontaly
     }
     void SetCameraCorrectAllowVerFalse()
     {
-        transform.position = new Vector3(transform.position.x, player.transform.position.y + cameraOffsetY, transform.position.z);
-        cameraCorrectAllowVer = false;
+        transform.position = new Vector3(transform.position.x, player.transform.position.y + cameraOffsetY, transform.position.z);   // correction position of camera
+        cameraCorrectAllowVer = false;  // forbid correct camera verticaly
     }
-    /**/
+    /*correction camera*/
     void CameraCorrectHorizontal()
     {
         if (cameraOffsetX < 0) // facing left
         {
-            if (transform.position.x - player.transform.position.x > cameraOffsetX)
+            if (transform.position.x - player.transform.position.x > cameraOffsetX)  
             {
                 transform.position = new Vector3(transform.position.x - cameraStepX, transform.position.y, transform.position.z); // transport camera to other side
+                
             }
             else
             {
@@ -85,9 +86,9 @@ public class CameraFollow : MonoBehaviour {
     }
     void CameraMoveVertical()
     {
-        if (allowMoveCameraVerUp)
+        if (allowMoveCameraUp)
             transform.position = new Vector3(transform.position.x, player.transform.position.y - cameraEdgeUp, transform.position.z); // following camera
-        if (allowMoveCameraVerDown)
+        if (allowMoveCameraDown)
             transform.position = new Vector3(transform.position.x, player.transform.position.y - cameraEdgeDown, transform.position.z); // following camera
     }
     
@@ -95,24 +96,24 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
-        if (anim.GetBool("wallAttached"))
-            allowMoveCameraHor = false;
+        if (anim.GetBool("wallAttached"))   // touching wall
+            allowMoveCameraHor = false;     // is stoping camera
         if (anim.GetBool("grounded"))
             cameraCorrectAllowVer = true;
-        if (transform.position.x - player.transform.position.x > cameraEdgeHor && cameraOffsetX > 0)
+        if (transform.position.x - player.transform.position.x > cameraEdgeHor && cameraOffsetX > 0) // player out of horizontal camera edge (after changing side) and facing right
         {
             cameraCorrectAllowHor = true;
             cameraOffsetX = -cameraOffsetX;
             allowMoveCameraHor = true;
         }
-        if (transform.position.x - player.transform.position.x < -cameraEdgeHor && cameraOffsetX < 0)
+        if (transform.position.x - player.transform.position.x < -cameraEdgeHor && cameraOffsetX < 0) // player out of horizontal camera edge (after changing side) and facing left
         {
             cameraCorrectAllowHor = true;
             cameraOffsetX = -cameraOffsetX;
             allowMoveCameraHor = true;
         }
 
-        if ((cameraOffsetX > 0 && player.transform.position.x - transform.position.x > cameraEdgeHor) || (cameraOffsetX < 0 && player.transform.position.x - transform.position.x < -cameraEdgeHor))
+        if ((cameraOffsetX > 0 && player.transform.position.x - transform.position.x > cameraEdgeHor) || (cameraOffsetX < 0 && player.transform.position.x - transform.position.x < -cameraEdgeHor))  // player out of camera edge without changing side
         {
             cameraCorrectAllowHor = true;
             allowMoveCameraHor = true;
@@ -120,22 +121,22 @@ public class CameraFollow : MonoBehaviour {
 
         if (player.transform.position.y - transform.position.y > cameraEdgeUp)
         {
-            allowMoveCameraVerUp = true;
-            allowMoveCameraVerDown = false;
+            allowMoveCameraUp = true;
+            allowMoveCameraDown = false;
             cameraCorrectAllowVer = false;
         }
         else
-            allowMoveCameraVerUp = false;
+            allowMoveCameraUp = false;
 
         if (player.transform.position.y - transform.position.y < cameraEdgeDown)
         {
-            allowMoveCameraVerDown = true;
-            allowMoveCameraVerUp = false;
+            allowMoveCameraDown = true;
+            allowMoveCameraUp = false;
             cameraCorrectAllowVer = false;
         }
         else
         {
-            allowMoveCameraVerDown = false;
+            allowMoveCameraDown = false;
         }
 
 
